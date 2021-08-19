9403dd44c8da72e5fb82242a273f36250ae7f13f
Subhodeep Das <dassubhodeep.10@gmail.com>
2021-08-19 13:05:14 +0530

    merge to head

---
+++ cdsp-extn/src/main/java/com/cdsp/cdspextn/CdspExtnApplication.java
@@ -0,0 +1,13 @@
+package com.cdsp.cdspextn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdspExtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdspExtnApplication.class, args);
+	}
+
+}
---
+++ cdsp-extn/src/test/java/com/cdsp/cdspextn/CdspExtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdsp.cdspextn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdspExtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
